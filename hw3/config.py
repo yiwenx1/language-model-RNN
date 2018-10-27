1bef45ae415945194af6d28d8f7bd6e38f66f9cf
Yiwen Xu <yiwen@Yiwens-MBP.wv.cc.cmu.edu>
2018-10-26 20:35:17 -0400

    add hw3

---
+++ hw3/config.py
@@ -0,0 +1,12 @@
+LANGUAGEMODEL_CONFIG = {
+    'seq_len': 200,
+    'lr': 0.01,
+    'nepochs': 3,
+    'batch_size': 16,
+    'embed_size': 256,
+    'hidden_size': 256,
+    'nlayers': 1
+}
+
+print(LANGUAGEMODEL_CONFIG)
+print(LANGUAGEMODEL_CONFIG['lr'])
